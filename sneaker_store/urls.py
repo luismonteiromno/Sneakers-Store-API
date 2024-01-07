3c22848deb37f2558ed2af946bc150b6f2c55201
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-06 22:37:02 -0300

    Upload of photo of sneaker fixed

--- sneaker_store/urls.py
+++ sneaker_store/urls.py
@@ -24,6 +24,5 @@ from .routers import router
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-]
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
