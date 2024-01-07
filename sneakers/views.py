e9378201256ab3a54320e1f03d1cde5ca7ba29a1
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-07 14:35:38 -0300

    Endpoints of register and update sneaker created

--- sneakers/views.py
+++ sneakers/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 
-from .models import Sneakers
+from .models import Sneakers, Brands, Lines
 from .serializers import SneakersSerializers
 
 
@@ -13,6 +14,99 @@ class SneakersViewSet(ModelViewSet):
     queryset = Sneakers.objects.all()
     serializer_class = SneakersSerializers
     permission_classes = IsAuthenticated
+
+    @action(detail=False, methods=['POST'], permission_classes=[IsAuthenticated])
+    def register_sneaker(self, request):
+        data = request.data
+        try:
+            available_sizes = data['available_sizes'].split(",")
+            sizes = []
+
+            for available_size in available_sizes:
+                sizes.append(available_size)
+
+            if data.get('line_id') != None and data.get('line_id') != ' ' and data.get('line_id') != '':
+                brand = Brands.objects.get(id=data['brand_id'])
+                line = Lines.objects.get(id=data.get('line_id'))
+
+                if line.brand_line != brand:
+                    return Response({'message': 'A linha escolhida não pertence a essa marca!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+                Sneakers.objects.create(
+                    photo=data['photo'],
+                    name=data['name'],
+                    price=data['price'],
+                    brand_id=data['brand_id'],
+                    line_id=data.get('line_id', None),
+                    model=data['model'],
+                    available_sizes=sizes
+                )
+
+            else:
+                Sneakers.objects.create(
+                    photo=data['photo'],
+                    name=data['name'],
+                    price=data['price'],
+                    brand_id=data['brand_id'],
+                    line_id=data.get('line_id', None),
+                    model=data['model'],
+                    available_sizes=sizes
+                )
+
+            return Response({'message': 'Tênis registrado com sucesso'}, status=status.HTTP_200_OK)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao registrar tênis!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['PATCH'], permission_classes=[IsAuthenticated])
+    def update_sneaker(self, request):
+        data = request.data
+        available_sizes = data['available_sizes'].split(",")
+        sizes = []
+        try:
+            if data.get('line_id') != None and data.get('line_id') != ' ' and data.get('line_id') != '':
+                brand = Brands.objects.get(id=data['brand_id'])
+                line = Lines.objects.get(id=data.get('line_id'))
+
+                if line.brand_line != brand:
+                    return Response({'message': 'A linha escolhida não pertence a essa marca!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                sneaker = Sneakers.objects.get(id=data['sneaker_id'])
+                sneaker.photo = data['photo']
+                sneaker.name = data['name']
+                sneaker.price = data['price']
+                sneaker.brand_id = data['brand_id']
+                sneaker.line_id = data.get('line_id')
+                sneaker.model = data['model']
+                if sneaker.available_sizes != available_sizes:
+                    sneaker.available_sizes.clear()
+                    for available_size in available_sizes:
+                        sizes.append(available_size)
+                        sneaker.available_sizes = sizes
+
+                sneaker.save()
+            else:
+                sneaker = Sneakers.objects.get(id=data['sneaker_id'])
+                sneaker.photo = data['photo']
+                sneaker.name = data['name']
+                sneaker.price = data['price']
+                sneaker.brand_id = data['brand_id']
+                sneaker.line_id = data.get('line_id')
+                sneaker.model = data['model']
+                if sneaker.available_sizes != available_sizes:
+                    sneaker.available_sizes.clear()
+                    for available_size in available_sizes:
+                        sizes.append(available_size)
+                        sneaker.available_sizes = sizes
+
+                sneaker.save()
+            return Response({'message': 'Tênis atualizado com sucesso'}, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'Tênis não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao atualizar tênis!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def list_sneakers(self, request):
@@ -23,3 +117,16 @@ class SneakersViewSet(ModelViewSet):
         except Exception as error:
             print(error)
             return Response({'message': 'Erro ao listar os tênis!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
+    def sneaker_by_id(self, request):
+        params = request.query_params
+        try:
+            sneakers = Sneakers.objects.get(id=params['sneaker_id'])
+            serializer = SneakersSerializers(sneakers)
+            return Response({'message': 'Tênis encontrado', 'sneaker': serializer.data}, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response({'message': 'Tênis não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao buscas o tênis!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
