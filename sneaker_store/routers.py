7f984da6725dcfce728d9790193e0d861796b972
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-08 23:24:50 -0300

    Router of Users API created

--- sneaker_store/routers.py
+++ sneaker_store/routers.py
@@ -1,6 +1,8 @@
 from rest_framework import routers
+from users.views import UserViewSet
 from sneakers.views import SneakersViewSet
 
 router = routers.DefaultRouter()
 
+router.register(r'users', UserViewSet, basename='users')
 router.register(r'sneakers', SneakersViewSet, basename='sneakers')
