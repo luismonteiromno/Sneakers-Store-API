e5a43f7d6cfa7eb9e43539b4a1b2e883fc5c33fb
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-08 23:41:23 -0300

    Sneakers favorites added in admin

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,7 @@ from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'email']
+    filter_horizontal = ['favorite_brands', 'favorite_sneakers']
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
