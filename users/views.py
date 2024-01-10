a7d851d550637fdb3ddcca1de5927bcc9631cfe1
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-09 21:41:56 -0300

    Endpoint of update user created

--- users/views.py
+++ users/views.py
@@ -44,6 +44,44 @@ class UserViewSet(ModelViewSet):
             print(error)
             return Response({'message': 'Erro ao registrar novo usuário!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    @action(detail=False, methods=['PATCH'], permission_classes=[IsAuthenticated])
+    def update_user(self, request):
+        user = request.user
+        data = request.data
+        try:
+            user_update = UserProfile.objects.get(id=user.id)
+            user_update.username = data['username']
+            user_update.first_name = data['first_name']
+            user_update.last_name = data['last_name']
+            user_update.full_name = data['full_name']
+            user_update.notification_active = data['notification_active']
+
+            if user_update.favorite_brands != data['favorite_brands']:
+                user_update.favorite_brands.clear()
+                for favorite_brand in data['favorite_brands']:
+                    user_update.favorite_brands.add(int(favorite_brand))
+
+            if user_update.favorite_sneakers != data['favorite_sneakers']:
+                user_update.favorite_sneakers.clear()
+                for favorite_sneaker in data['favorite_sneakers']:
+                    user_update.favorite_sneakers.add(int(favorite_sneaker))
+
+            if user_update.cpf != data['cpf']:
+                if UserProfile.objects.filter(cpf=data['cpf']).exists():
+                    return Response({'message': 'CPF inválido!'}, status=status.HTTP_400_BAD_REQUEST)
+                user_update.cpf = data['cpf']
+
+            if user_update.email != data['email']:
+                if UserProfile.objects.filter(email=data['email']).exists():
+                    return Response({'message': 'Este email já está sendo utilizado!'}, status=status.HTTP_400_BAD_REQUEST)
+                user_update.email = data['email']
+
+            user_update.save()
+            return Response({'message': 'Perfil atualizado com sucesso'}, status=status.HTTP_200_OK)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao atualizar perfil!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def get_user(self, request):
         user = request.user
@@ -54,3 +92,4 @@ class UserViewSet(ModelViewSet):
         except Exception as error:
             print(error)
             return Response({'message': 'Erro ao exibir perfil do usuário!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
