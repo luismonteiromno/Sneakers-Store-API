74369d12f9ca4690fdfd7f761d0a810f670b729c
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-09 22:48:34 -0300

    New feature purchases created

--- purchases/models.py
+++ purchases/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import UserProfile
+from sneakers.models import Sneakers
 
-# Create your models here.
+
+class Purchases(models.Model):
+    sneaker = models.ManyToManyField(Sneakers, verbose_name='Tênis Comprado')
+    user = models.ForeignKey(UserProfile, verbose_name='Usuário', related_name='user_purchase', on_delete=models.CASCADE)
+    date_purchase = models.DateTimeField('Data da Compra', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {str(self.date_purchase)}"
+
+    class Meta:
+        verbose_name = 'Compra'
+        verbose_name_plural = 'Compras'
