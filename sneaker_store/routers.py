a77121ae87125a7285434905de97ad205727f73b
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-09 22:49:45 -0300

    Route of API purchases added

--- sneaker_store/routers.py
+++ sneaker_store/routers.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 from users.views import UserViewSet
 from sneakers.views import SneakersViewSet
+from purchases.views import PurchasesViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'sneakers', SneakersViewSet, basename='sneakers')
+router.register(r'purchases', PurchasesViewSet, basename='purchases')
