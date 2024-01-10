98d8ae5b370ce9a7328ee2a54ae4e111964c6d44
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-09 22:48:54 -0300

    New feature purchases added in admin

--- purchases/admin.py
+++ purchases/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Purchases
 
-# Register your models here.
+admin.site.register(Purchases)
--- purchases/apps.py
+++ purchases/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PurchasesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'purchases'
+    verbose_name = 'Compras'
