44c939f42cab2b6be0f747194450564fddf57850
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-18 23:13:59 -0300

    Now users can have type

--- users/models.py
+++ users/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from sneakers.models import Brands, Sneakers
 
+TYPE_USERS = (
+    ('client', 'Cliente'),
+    ('employee', 'Funcionário'),
+    ('Admin', 'Admin')
+)
+
 
 class UserProfile(AbstractUser):
     username = models.CharField('Nome de Usuário')
@@ -10,6 +16,7 @@ class UserProfile(AbstractUser):
     full_name = models.CharField('Nome completo', max_length=200)
     cpf = models.CharField('CPF', unique=True, max_length=11)
     email = models.EmailField('Email', unique=True)
+    type_user = models.CharField('Tipo do Usuário', choices=TYPE_USERS)
     favorite_brands = models.ManyToManyField(Brands, verbose_name='Marcas Favoritas', blank=True)
     favorite_sneakers = models.ManyToManyField(Sneakers, verbose_name='Tênis Favoritos', blank=True)
     notification_active = models.BooleanField('Notificações Ativas', default=False)
