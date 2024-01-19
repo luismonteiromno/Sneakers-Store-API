38b11f38f7a24190a563f2ae0a766b0dd54da3af
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-18 23:14:15 -0300

    Filter user by type in admin

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,7 @@ from .models import UserProfile
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'email']
     filter_horizontal = ['favorite_brands', 'favorite_sneakers']
+    list_filter = ['type_user']
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
