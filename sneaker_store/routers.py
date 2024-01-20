80e9e0a134d93a87371efe8ba9d7cad1ab4acb03
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-20 15:37:39 -0300

    Route of endpoints of adverts created

--- sneaker_store/routers.py
+++ sneaker_store/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from users.views import UserViewSet
 from store.views import StoreViewSet
-from sneakers.views import SneakersViewSet
+from sneakers.views import SneakersViewSet, AdvertsViewSet
 from purchases.views import PurchasesViewSet
 from type_payments.views import TypePaymentsViewSet
 
@@ -10,5 +10,6 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'stores', StoreViewSet, basename='stores')
 router.register(r'sneakers', SneakersViewSet, basename='sneakers')
+router.register(r'adverts', AdvertsViewSet, basename='adverts')
 router.register(r'purchases', PurchasesViewSet, basename='purchases')
 router.register(r'type_payments', TypePaymentsViewSet, basename='type_payments')
