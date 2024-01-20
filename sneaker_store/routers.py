b68e56e691fa8d5c5ad4f7a7a01a3278c26eab2b
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-19 22:30:51 -0300

    Route of API store created

--- sneaker_store/routers.py
+++ sneaker_store/routers.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from users.views import UserViewSet
+from store.views import StoreViewSet
 from sneakers.views import SneakersViewSet
 from purchases.views import PurchasesViewSet
 from type_payments.views import TypePaymentsViewSet
@@ -7,6 +8,7 @@ from type_payments.views import TypePaymentsViewSet
 router = routers.DefaultRouter()
 
 router.register(r'users', UserViewSet, basename='users')
+router.register(r'stores', StoreViewSet, basename='stores')
 router.register(r'sneakers', SneakersViewSet, basename='sneakers')
 router.register(r'purchases', PurchasesViewSet, basename='purchases')
 router.register(r'type_payments', TypePaymentsViewSet, basename='type_payments')
