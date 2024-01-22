7e4302c75f53f06412b61950b3741ca1ad7ffb0a
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-21 21:14:46 -0300

    Sneakers field fixed in admin

--- sneakers/admin.py
+++ sneakers/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Sneakers, Brands, Lines, Adverts
+from django import forms
+from django.forms import ValidationError
 
 
 class BrandsAdmin(admin.ModelAdmin):
@@ -11,11 +13,22 @@ class SneakersAdmin(admin.ModelAdmin):
     list_filter = ['brand', 'line', 'in_stock']
 
 
+class AdvertsForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data = super().clean()
+        create_at = cleaned_data.get('create_at')
+        expiration = cleaned_data.get('expiration')
+        if create_at >= expiration:
+            raise ValidationError('A data de criação não pode ser maior/igual a data de expiração!')
+
+
 class AdvertsAdmin(admin.ModelAdmin):
     fieldsets = (
-        ('Informações do anúncio', {'fields': ('sneaker', 'advert', 'create_at', 'expiration')}),
+        ('Informações do anúncio', {'fields': ('sneaker', 'advert', 'description', 'create_at', 'expiration')}),
     )
-    list_display = ['sneaker', 'create_at', 'expiration']
+    list_display = ['id', 'create_at', 'expiration']
+    filter_horizontal = ['sneaker']
+    form = AdvertsForm
 
 
 admin.site.register(Sneakers, SneakersAdmin)
