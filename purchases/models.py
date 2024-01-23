5a225784d5af19d2652a2c3b4d3b6e4e07b520be
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-23 20:01:06 -0300

    Type payment added in purchases

--- purchases/models.py
+++ purchases/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from users.models import UserProfile
 from sneakers.models import Sneakers
+from type_payments.models import TypePayments
 
 
 class Purchases(models.Model):
     sneaker = models.ManyToManyField(Sneakers, verbose_name='Tênis Comprado')
     user = models.ForeignKey(UserProfile, verbose_name='Usuário', related_name='user_purchase', on_delete=models.CASCADE)
+    type_payment = models.ForeignKey(TypePayments, verbose_name='Tipo de pagamento', related_name='purchase_type_payment', on_delete=models.CASCADE)
     date_purchase = models.DateTimeField('Data da Compra', auto_now_add=True)
 
     def __str__(self):
