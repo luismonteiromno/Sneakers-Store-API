f603c0d14670353d0e159f768509587c64288b67
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-26 22:34:28 -0300

    Endpoint of update store created

--- store/views.py
+++ store/views.py
@@ -84,6 +84,79 @@ class StoreViewSet(ModelViewSet):
             print(error)
             return Response({'message': 'Erro ao registrar loja!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    @action(detail=False, methods=['PATCH'], permission_classes=[IsAuthenticated])
+    def update_store(self, request):
+        user = request.user
+        data = request.data
+        try:
+            invalids_values = [None, ' ', '']
+            store = Store.objects.get(id=data['store_id'])
+            if user in store.owner.all():
+                store.name = data['name']
+                store.street = data['street']
+                store.number = data['number']
+                store.state = data['state']
+
+                if data['cnpj'] != store.cnpj:
+                    if Store.objects.filter(cnpj=data['cnpj']).exists():
+                        return Response({'message': 'Este CNPJ já está sendo utilizado!'}, status=status.HTTP_400_BAD_REQUEST)
+                    store.cnpj = data['cnpj']
+
+                if data['email'] != store.email:
+                    if Store.objects.filter(email=data['email']).exists():
+                        return Response({'message': 'Este EMAIL já está sendo utilizado!'}, status=status.HTTP_400_BAD_REQUEST)
+                    store.email = data['email']
+
+                store.facebook = data.get('facebook', None)
+                store.instagram = data.get('instagram', None)
+                store.whatsapp = data.get('whatsapp', None)
+
+                if data['owners'] != store.owner:
+                    store.owner.clear()
+                    for owner in data['owners']:
+                        store.owner.add(owner)
+
+                if data['products'] != store.products:
+                    store.products.clear()
+                    for product in data['products']:
+                        store.products.add(product)
+
+                if data['type_payments'] != store.type_payments:
+                    store.type_payments.clear()
+                    for type_payment in data['type_payments']:
+                        store.type_payments.add(type_payment)
+
+                store.delivery = data['delivery']
+                store.minimum_delivery = data['minimum_delivery']
+                store.maximum_delivery = data['maximum_delivery']
+
+                if data['delivery'] == True and (
+                        data['minimum_delivery'] in invalids_values or data['maximum_delivery'] in invalids_values
+                ):
+                    return Response({'message': 'O campo tempo minímo/máximo de entrega deve ser preenchido!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+                if data['delivery'] == False and (
+                        data['minimum_delivery'] not in invalids_values or data['maximum_delivery'] not in invalids_values
+                ):
+                    return Response({
+                                        'message': 'Preencha o campo de Entrega que os campos de tempo minímo/máximo de entrega possa(m) ser preenchido(s)!'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+                if data['minimum_delivery'] not in invalids_values and data['maximum_delivery'] not in invalids_values and (
+                        data['minimum_delivery'] >= data['maximum_delivery']
+                ):
+                    return Response({'message': 'Preencha o campo tempo minímo/máximo de entrega corretamente!'}
+                                    , status=status.HTTP_400_BAD_REQUEST)
+
+                store.save()
+                return Response({'message': 'Loja atualizada com sucesso'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Somente o(s) dono(s) pode(m) realizar esta ação!'}, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao atualizar dados da loja!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['GET'], permission_classes=[IsAuthenticated])
     def user_stores(self, request):
         user = request.user
