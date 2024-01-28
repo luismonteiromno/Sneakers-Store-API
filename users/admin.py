2c0905515f7e1327ba39bd1617abf4e1158bbe9a
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-27 21:48:36 -0300

    Full name fields are now auto-populated when saving the table in admin

--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,14 @@ from .models import UserProfile
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['id', 'full_name', 'email']
+    list_display = ['id', 'username', 'email']
     filter_horizontal = ['favorite_brands', 'favorite_sneakers']
     list_filter = ['type_user']
+
+    def save_model(self, request, obj, form, change):
+        full_name = obj.first_name + ' ' + obj.last_name
+        obj.full_name = full_name
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
