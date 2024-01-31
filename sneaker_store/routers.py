eefc1fefe127274e400ed815d0caba139a331b96
Luis Monteiro <felipemoreno9065@gmail.com>
2024-01-30 23:14:55 -0300

    Route of endpoints of brands created

--- sneaker_store/routers.py
+++ sneaker_store/routers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from users.views import UserViewSet
 from store.views import StoreViewSet
-from sneakers.views import SneakersViewSet, AdvertsViewSet
+from sneakers.views import SneakersViewSet, AdvertsViewSet, BrandsViewSet
 from purchases.views import PurchasesViewSet
 from type_payments.views import TypePaymentsViewSet
 
@@ -10,6 +10,7 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 router.register(r'stores', StoreViewSet, basename='stores')
 router.register(r'sneakers', SneakersViewSet, basename='sneakers')
+router.register(r'brands', BrandsViewSet, basename='brands')
 router.register(r'adverts', AdvertsViewSet, basename='adverts')
 router.register(r'purchases', PurchasesViewSet, basename='purchases')
 router.register(r'type_payments', TypePaymentsViewSet, basename='type_payments')
