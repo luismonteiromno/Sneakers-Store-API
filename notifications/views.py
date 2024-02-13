2aa46b94e40f4a77347cc7ef36e078eba929ade3
Luis Monteiro <felipemoreno9065@gmail.com>
2024-02-12 22:47:43 -0300

    Sentry monitoring added

--- notifications/views.py
+++ notifications/views.py
@@ -7,22 +7,46 @@ from rest_framework.decorators import action
 
 from .models import Notifications
 
+from users.models import UserProfile
+
 from .serializers import NotificationsSerializers
 
+from datetime import datetime
+import sentry_sdk
 
 class NotificationsViewSet(ModelViewSet):
     queryset = Notifications.objects.all()
     serializer_class = NotificationsSerializers
     permission_classes = AllowAny
 
+    @action(detail=False, methods=['POST'], permission_classes=[IsAuthenticated])
+    def send_notification(self, request):
+        data = request.data
+        now = datetime.now()
+        try:
+            email = data['email']
+            if UserProfile.objects.filter(email=email).exists():
+                Notifications.objects.create(
+                    email=email,
+                    subject=data['subject'],
+                    message=data['message'],
+                    send_date=now
+                )
+                return Response({'message': 'Notificação enviada com sucesso'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Email não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            print(error)
+            return Response({'message': 'Erro ao enviar notificação!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     @action(detail=False, methods=['GET'], permission_classes=[AllowAny])
     def list_notifications(self, request):
         try:
-            notifications = self.queryset
+            notifications = Notifications.objects.all()
             serializer = NotificationsSerializers(notifications, many=True)
             return Response({'message': 'Notificações encontradas', 'notifications': serializer.data}, status=status.HTTP_200_OK)
         except Exception as error:
-            print(error)
+            sentry_sdk.capture_exception(error)
             return Response({'message': 'Erro ao listar notificações'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     @action(detail=False, methods=['PUT'], permission_classes=[IsAuthenticated])
