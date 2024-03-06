4dfff80cf296526dd76cbfacde96fa65f2d6a139
Luis Monteiro <felipemoreno9065@gmail.com>
2024-03-06 18:56:01 -0300

    When making a purchase, a notification is sent to the store owner

--- notifications/models.py
+++ notifications/models.py
@@ -5,6 +5,7 @@ class Notifications(models.Model):
     email = models.EmailField('Email')
     subject = models.CharField('Titulo')
     message = models.TextField('Mensagem', max_length=255)
+    purchase_notification = models.BooleanField('Notificação de compra', default=False)
     is_read = models.BooleanField('Foi lida', default=False)
     send_date = models.DateTimeField('Data de envio', auto_now_add=True, null=True, blank=True)
 
